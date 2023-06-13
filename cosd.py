22174beb07eb80989ec518f1154a45ae29509a20
Onderglmz <81230504+Onderglmz@users.noreply.github.com>
2023-06-13 18:34:26 +0300

    Update cosd.py

--- cosd.py
+++ cosd.py
@@ -30,7 +30,7 @@ skin_df = pd.read_csv('data/HAM10000/HAM10000_metadata.csv')
 
 SIZE=32
 
-# label encoding to numeric values from text
+
 le = LabelEncoder()
 le.fit(skin_df['dx'])
 LabelEncoder()
@@ -40,8 +40,8 @@ skin_df['label'] = le.transform(skin_df["dx"])
 print(skin_df.sample(10))
 
 
-# Data distribution visualization
-fig = plt.figure(figsize=(15,10))
+
+fig = plt.figure(figsize=(14,10))
 
 ax1 = fig.add_subplot(221)
 skin_df['dx'].value_counts().plot(kind='bar', ax=ax1)
@@ -68,7 +68,7 @@ plt.tight_layout()
 plt.show()
 
 
-# Distribution of data into various classes 
+
 from sklearn.utils import resample
 print(skin_df['label'].value_counts())
 
@@ -76,52 +76,52 @@ print(skin_df['label'].value_counts())
 # Many ways to balance data... you can also try assigning weights during model.fit
 #Separate each classes, resample, and combine back into single dataframe
 
-df_0 = skin_df[skin_df['label'] == 0]
-df_1 = skin_df[skin_df['label'] == 1]
-df_2 = skin_df[skin_df['label'] == 2]
-df_3 = skin_df[skin_df['label'] == 3]
-df_4 = skin_df[skin_df['label'] == 4]
-df_5 = skin_df[skin_df['label'] == 5]
-df_6 = skin_df[skin_df['label'] == 6]
+test_0  = skin_df[skin_df['label'] == 0]
+test_1 = skin_df[skin_df['label'] == 1]
+test_2 = skin_df[skin_df['label'] == 2]
+test_3 = skin_df[skin_df['label'] == 3]
+test_4= skin_df[skin_df['label'] == 4]
+test_5 = skin_df[skin_df['label'] == 5]
+test_6 = skin_df[skin_df['label'] == 6]
 
-n_samples=700 
-df_0_balanced = resample(df_0, replace=True, n_samples=n_samples, random_state=42) 
-df_1_balanced = resample(df_1, replace=True, n_samples=n_samples, random_state=42) 
-df_2_balanced = resample(df_2, replace=True, n_samples=n_samples, random_state=42)
-df_3_balanced = resample(df_3, replace=True, n_samples=n_samples, random_state=42)
-df_4_balanced = resample(df_4, replace=True, n_samples=n_samples, random_state=42)
-df_5_balanced = resample(df_5, replace=True, n_samples=n_samples, random_state=42)
-df_6_balanced = resample(df_6, replace=True, n_samples=n_samples, random_state=42)
+n_samples=1000 
+test_0_balanced = resample(test_0, replace=True, n_samples=n_samples, random_state=42) 
+test_1_balanced = resample(test_1, replace=True, n_samples=n_samples, random_state=42) 
+test_2_balanced = resample(test_2, replace=True, n_samples=n_samples, random_state=42)
+test_3_balanced = resample(test_3, replace=True, n_samples=n_samples, random_state=42)
+test_4_balanced = resample(test_4, replace=True, n_samples=n_samples, random_state=42)
+test_5_balanced = resample(test_5, replace=True, n_samples=n_samples, random_state=42)
+test_6_balanced = resample(test_6, replace=True, n_samples=n_samples, random_state=42)
 
-skin_df_balanced = pd.concat([df_0_balanced, df_1_balanced, 
-                              df_2_balanced, df_3_balanced, 
-                              df_4_balanced, df_5_balanced, df_6_balanced])
+skin_test_balanced = pd.concat([test_0_balanced, test_1_balanced, 
+                              test_2_balanced, test_3_balanced, 
+                              test_4_balanced, test_5_balanced, test_6_balanced])
 
 
-print(skin_df_balanced['label'].value_counts())
+print(skin_test_balanced['label'].value_counts())
 
 
 image_path = {os.path.splitext(os.path.basename(x))[0]: x
                      for x in glob(os.path.join('data/HAM10000/', '*', '*.jpg'))}
 
-skin_df_balanced['path'] = skin_df['image_id'].map(image_path.get)
+skin_test_balanced['path'] = skin_df['image_id'].map(image_path.get)
 
-skin_df_balanced['image'] = skin_df_balanced['path'].map(lambda x: np.asarray(Image.open(x).resize((SIZE,SIZE))))
+skin_test_balanced['image'] = skin_df_balanced['path'].map(lambda x: np.asarray(Image.open(x).resize((SIZE,SIZE))))
 
 
-n_samples = 5  
+n_samples = 7  
 
 
 fig, m_axs = plt.subplots(7, n_samples, figsize = (4*n_samples, 3*7))
 for n_axs, (type_name, type_rows) in zip(m_axs, 
-                                         skin_df_balanced.sort_values(['dx']).groupby('dx')):
+                                         skin_test_balanced.sort_values(['dx']).groupby('dx')):
     n_axs[0].set_title(type_name)
     for c_ax, (_, c_row) in zip(n_axs, type_rows.sample(n_samples, random_state=1234).iterrows()):
         c_ax.imshow(c_row['image'])
         c_ax.axis('off')
 
 
-X = np.asarray(skin_df_balanced['image'].tolist())
+X = np.asarray(skin_test_balanced['image'].tolist())
 X = X/255. # Scale values to 0-1. You can also used standardscaler or other scaling methods.
 Y=skin_df_balanced['label'] 
 Y_cat = to_categorical(Y, num_classes=7) #Convert to categorical as this is a multiclass classification problem
